1944714b70ec81f4cfcab94deadfc1a34c255177
peter707 <peter707@vt.edu>
2017-03-01 13:47:33 -0500

    add more data to be scraped

--- data/scrape_statement.py
+++ data/scrape_statement.py
@@ -1,11 +1,26 @@
 import pdfkit
 import sys
 from bs4 import BeautifulSoup
-
-
+import urllib2
+import re
 config = pdfkit.configuration(wkhtmltopdf='./wkhtmltox/bin/wkhtmltopdf')
 def get_problem_statement(url,output_file):
-    pd = pdfkit.from_url(url,output_file,configuration=config)
+    url_results = urllib2.urlopen(url).read()
+    soup = BeautifulSoup(url_results)
+    val = soup.find("div", attrs={"class": re.compile('ttypography')})
+    head_val = soup.find("head")
+    for v in head_val.findAll("script"):
+      v.extract()
+    for v in head_val.findAll("link"):
+      v.extract()
+    for v in val.findAll("div", attrs= {"class" : re.compile('sample-tests')}):
+      v.extract()
+    val.find("div", attrs={"class" : re.compile("title")}).extract()
+    val.find("div", attrs={"class" :  re.compile("time-limit")}).extract()
+    val.find("div", attrs={"class" :  re.compile("memory-limit")}).extract()
+    val.find("div", attrs={"class" :  re.compile("input-file")}).extract()
+    val.find("div", attrs={"class" :  re.compile("output-file")}).extract()
+    pdfkit.from_string("<html>" + unicode(head_val.prettify()) + "<body>" + unicode(val.prettify()) + "</body></html>", output_file, configuration=config)
 
 if __name__ == "__main__":
 	argv = sys.argv
