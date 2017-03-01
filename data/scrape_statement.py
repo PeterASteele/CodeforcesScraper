68a58f95368e2fb8e2b9a6918d3d564a50ffe6cc
ericrw96 <ericrw96@vt.edu>
2017-03-01 11:21:04 -0500

    add parameters to the statement scraping script

--- data/scrape_statement.py
+++ data/scrape_statement.py
@@ -1,12 +1,12 @@
 import pdfkit
 import sys
 from bs4 import BeautifulSoup
-import urllib2
 
 
 config = pdfkit.configuration(wkhtmltopdf='./wkhtmltox/bin/wkhtmltopdf')
 def get_problem_statement(url,output_file):
     pd = pdfkit.from_url(url,output_file,configuration=config)
 
-
-get_problem_statement("http://codeforces.com/contest/778/problem/A","A_file.pdf")
+if __name__ == "__main__":
+	argv = sys.argv
+	get_problem_statement(sys.argv[1],sys.argv[2])
