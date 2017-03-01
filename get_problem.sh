91959f49dddebb3d53533cd6ad22c0107bd5b454
peter707 <peter707@vt.edu>
2017-03-01 12:43:56 -0500

    add a usage and customized title

--- get_problem.sh
+++ get_problem.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+# USAGE:submission_url problem_url problem_name
 source install_venv_and_packages.sh
 cd data
 ./bash_script_script.sh "$1"
 ./get_problem_pdf.sh "$2"
 cd ../problem_statement
-./gen_problem_statement.sh "sample title"
+./gen_problem_statement.sh "$3"
